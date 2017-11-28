b24e25b0746f26af7c7a0a7a8e7e66bb4a637eb0
siva Kumar ravula <sivaravula@health.usf.edu>
2017-11-28 00:14:17 -0500

    commit

---
+++ WebFormsApplication/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="Home Page" Language="C#"  AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormsApplication._Default"  %>
+
+<asp:Content runat="server" ID="HeadContent" ContentPlaceHolderID="HeadContent">
+    <script type="text/javascript">
+        setTimeout(function () {
+            document.getElementById('message').innerText = "Here are all of the global departments";
+        }, 2000);
+        
+    </script>
+
+</asp:Content>
+<asp:Content runat="server" ID="HeaderContent" ContentPlaceHolderID="HeaderContent">
+    Additional Header Content Added By Individual Application
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    
+        <h2 id="message"></h2>
+
+        <div id="Div1" runat="server">
+
+
+        </div>
+
+  
+</asp:Content>
+
+<asp:Content runat="server" ID="FooterContent" ContentPlaceHolderID="FooterContent">
+    Footer content added by individual application.
+    </asp:Content>
---
+++ WebFormsApplication/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+using System.Data.OracleClient;
+using System.Data;
+using System.Net;
+using System.Text;
+
+namespace WebFormsApplication
+{
+    public partial class _Default : Page
+    {
+        protected override void OnPreInit(EventArgs e)
+        {
+            //new GlobalTemplate().Render(System.IO.Path.Combine(MapPath("~/"), "globaltemplate.master"));
+              
+            MasterPageFile = MasterPageVirtualPathProvider.MasterPageFileLocation;
+            //MasterPageFile = "~/globaltemplate.master";
+            base.OnPreInit(e);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cmd = new OracleCommand();
+            cmd.CommandText = "select global_dept_name from hart_global_depts_mv";
+            var set = Database.GetData(cmd, "dbdev2.hscnet.hsc.usf.edu", "1522", "dbdev", "hsc", "graham_hill");
+
+            foreach (System.Data.DataRow rItem in set.Tables[0].Rows)
+            {
+                this.Div1.InnerHtml  += rItem["global_dept_name"].ToString() +"<br/>";
+            }
+        }
+        
+    }
+}
